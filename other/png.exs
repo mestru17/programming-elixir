f14abf864fd92f5a4148b754574eea6347e0a0d4
Mathias Estrup <32955302+mestru17@users.noreply.github.com>
2026-01-03 20:56:53 +0100

    refactor png module

--- other/png.exs
+++ other/png.exs
@@ -1,44 +1,36 @@
 defmodule Png do
-  defp png_header?(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>), do: true
-  defp png_header?(_), do: false
-
-  defp match_header!(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>), do: nil
-  defp match_header!(_), do: raise("invalid png header")
-
-  defp read_header(device), do: IO.binread(device, 8)
-
-  defp read_chunk(device) do
-    with <<length::size(32), type::binary-size(4)>> <- IO.binread(device, 8),
-         <<data::binary-size(length), crc::binary-size(4)>> <- IO.binread(device, length + 4) do
-      <<length::size(32), type::binary-size(4), data::binary-size(length), crc::binary-size(4)>>
+  defp header!(device) do
+    case IO.binread(device, 8) do
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>> -> nil
+      _ -> raise("invalid PNG header")
     end
   end
 
-  defp print_chunks(_device, :eof), do: nil
-
-  defp print_chunks(
-         device,
-         <<length::size(32), type::binary-size(4), data::binary-size(length),
-           crc::binary-size(4)>>
-       ) do
-    IO.puts("length: #{length}")
-    IO.puts("type: #{type}")
-    IO.puts("data: 0x#{Base.encode16(data)}")
-    IO.puts("crc: 0x#{Base.encode16(crc)}")
-    IO.puts("")
-    print_chunks(device, read_chunk(device))
+  defmodule Chunk do
+    defstruct length: 0, type: "", data: "", crc: ""
   end
 
-  defp print_chunks(device), do: print_chunks(device, read_chunk(device))
+  defp chunk(device) do
+    with <<length::size(32), type::binary-size(4)>> <- IO.binread(device, 8),
+         <<data::binary-size(length), crc::binary-size(4)>> <- IO.binread(device, length + 4) do
+      %Chunk{length: length, type: type, data: data, crc: crc}
+    end
+  end
 
-  def print(path) do
-    File.open!(path, fn device ->
-      read_header(device) |> match_header!()
-      print_chunks(device)
+  defp chunks!(device) do
+    Stream.unfold(device, fn device ->
+      case chunk(device) do
+        :eof -> nil
+        {:error, reason} -> raise("failed to read chunk: #{reason}")
+        chunk -> {chunk, device}
+      end
     end)
   end
 
-  def png?(path) do
-    File.open!(path, &read_header/1) |> png_header?()
+  def inspect_chunks!(path) do
+    File.open!(path, fn device ->
+      header!(device)
+      chunks!(device) |> Enum.each(&IO.inspect/1)
+    end)
   end
 end
